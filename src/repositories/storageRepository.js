9adce80e83658c18360c94486b31c7cda3d40af8
Salehahmedeus <ahmedfox99999@gmail.com>
2026-01-24 23:41:12 +0200

    feat: add salary storage support

--- src/repositories/storageRepository.js
+++ src/repositories/storageRepository.js
@@ -1,4 +1,5 @@
 const STORAGE_KEY = 'my-expenses-v1';
+const SALARY_KEY = 'my-salary-v1';
 
 export const storageRepository = {
   load() {
@@ -8,5 +9,14 @@ export const storageRepository = {
 
   save(data) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
+  },
+
+  loadSalary() {
+    const data = localStorage.getItem(SALARY_KEY);
+    return data ? parseFloat(data) : 0;
+  },
+
+  saveSalary(salary) {
+    localStorage.setItem(SALARY_KEY, salary.toString());
   }
 };
